3c0fc5871f1ba4d9b8a9fdc4c01f0962872ae114
pratiksha rasal <pratiksha.r.rasal@gmail.com>
2021-09-29 08:48:50 -0700

    Pooja

---
+++ src/test/java/cucumbermap/TestingHello.java
@@ -0,0 +1,18 @@
+package cucumbermap;
+
+public class TestingHello 
+{
+   public void hello()
+   {
+	   System.out.println("Hello as Testing purpose");
+   }
+
+   public static void main(String[]args)
+   {
+	   TestingHello obj=new TestingHello();
+	    obj.hello();
+	   
+   }
+
+
+}
